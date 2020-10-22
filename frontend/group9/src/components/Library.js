62818990613e9dad734890b014713338f08bbb08
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2020-10-21 19:50:39 -0700

    library functionality

--- frontend/group9/src/components/Library.js
+++ frontend/group9/src/components/Library.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import './Library.css';
 import { fetchLibrary } from '../actions';
 
 const Library = () => {
@@ -17,13 +18,21 @@ const Library = () => {
     return (
       librarySongs.map(song => {
         return (
-          <li>{song}</li>
+          <li className="song-item">
+            <p>{song.name} </p>
+            <p>{song.genre}</p>
+            <p>{song.duration}</p>
+            <p>{song.bars} bars</p>
+            <p>{song.rating}/10</p>
+            <p>{song.likes} People Liked it </p>  
+          </li>
         )
       })
     )
   }
   return(
     <div>
+      <h1>Music Library</h1>
       <ul>
         {renderSongs()}
       </ul>
