56aeadfab7f03dc35d9095e981bc87d4cc6a0c39
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2020-10-28 15:42:47 -0700

    some styling changes

--- frontend/group9/src/components/Main.js
+++ frontend/group9/src/components/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Button from '@material-ui/core/Button';
 
 const Main = () => {
   return(
     <div>
       <Link to="/library" >
-        <button>Library</button>
+        <Button color="primary" size="large">Library</Button>
       </Link>
     </div>
   )
