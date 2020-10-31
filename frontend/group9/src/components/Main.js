f94c915d6ffdbb810235745970df22dc03c7ac24
haostonh <hsthuynh@ucdavis.edu>
2020-10-30 22:34:31 -0700

    Finished the home page layout for different views

--- frontend/group9/src/components/Main.js
+++ frontend/group9/src/components/Main.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import './Main.css';
 
 const Main = () => {
   return(
     <div>
-      <Link to="/library" >
-        <Button color="primary" size="large">Library</Button>
-      </Link>
+      <Helmet>
+        <style>{'body { background-color: #fff3b6 }'}</style>
+      </Helmet>
+      <div class="main-title">Library</div>
+      <div class="main-center-container">
+        <div class="main-text-container">
+          <div class="main-center-title">MusicSynth</div>
+          <div class="main-center-subtitle">AI that makes music for you</div>
+        </div>
+        <Link to="/library" >
+          <Button class="main-generate-button" color="primary" size="large">Generate!</Button>
+        </Link>
+      </div>
     </div>
   )
 }
