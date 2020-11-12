dfede35001f94d133be1ff5a63121e699790013f
Jerry Kuo <jerrykuo820@gmail.com>
2020-11-12 15:45:13 -0800

    Time Step Parsing

--- data_mining/midi_process.py
+++ data_mining/midi_process.py
@@ -1,48 +1,43 @@
-from music21 import *
+import numpy as np
+import pandas as pd
+from math import ceil
 import os
 import pretty_midi
 
-##midi_source = './local_MIDI'
-##
-##for midi_ in os.listdir(midi_source):
-##    path = os.path.join(midi_source, midi_)
-##
-##    mf = midi.MidiFile()
-##    mf.open(path)
-##
-##    """do stuff here lol"""
-##
-##    mf.read()
-##    mf.close()
+midi_dir = './local_MIDI/'
+parsed_dir = './parsed_data/'
+delta = 0.07
 
-import pretty_midi
-# Load MIDI file into PrettyMIDI object
-midi_data = pretty_midi.PrettyMIDI('Test.mid')
-# Print an empirical estimate of its global tempo
-print(midi_data.estimate_tempo())
-# Compute the relative amount of each semitone across the entire song, a proxy for key
-total_velocity = sum(sum(midi_data.get_chroma()))
-print([sum(semitone)/total_velocity for semitone in midi_data.get_chroma()])
-# Shift all notes up by 5 semitones
-print(midi_data.instruments)
-##for instrument in midi_data.instruments:
-##    # Don't want to shift drum notes
-##    if not instrument.is_drum:
-##        for note in instrument.notes:
-##            note.pitch += 5
-### Synthesize the resulting MIDI data using sine waves
-##audio_data = midi_data.synthesize()
+for filename in os.listdir(midi_dir):
+    _filename = os.path.splitext(filename)[0]
+    midi_data = pretty_midi.PrettyMIDI(midi_dir + filename)
 
-f = open("parsedMidi.txt", "w")
-for instrument in midi_data.instruments:
-    print(instrument.name)
-    f.write( "\n" +str(instrument.name) )
-    for i,note in enumerate(instrument.notes):
-        print(note)
-        f.write( "\n" +str(note))
-        if (i>5):
-            break
+    # Print an empirical estimate of its global tempo
+    # print(midi_data.estimate_tempo())
 
-f.close()
+    # Compute the relative amount of each semitone across the entire song, a proxy for key
+    # total_velocity = sum(sum(midi_data.get_chroma()))
+    # print([sum(semitone) / total_velocity for semitone in midi_data.get_chroma()])
 
+    # Shift all notes up by 5 semitones
+    # print(midi_data.instruments)
+
+    with open(parsed_dir + _filename + '.txt', "w") as f:
+        print(_filename)
+        f.write("\"" + _filename + "\"" + '\n\n')
+
+        for instrument in midi_data.instruments:
+            # print(instrument.name)
+            f.write("# " + str(instrument.name) + '\n')
+
+            song_length = instrument.notes[-1].end
+            intervals = int(song_length / delta)
+            time_stamp = np.linspace(0, intervals * delta, intervals + 1)
+
+            for note in instrument.notes:
+                taken_spaces = int(note.duration / delta)  # calculate the space required for the duration
+                ptr = ceil(note.start / delta)
+                for i in range(taken_spaces):
+                    string = '%f %d %d\n' % (time_stamp[ptr + i], note.pitch, note.velocity)
+                    f.write(string)
 
--- data_mining/parse_aux/utils.py
+++ data_mining/parse_aux/utils.py
@@ -1,26 +1,4 @@
 """Helper Parsing Functions for ML Team"""
-import numpy as np
-
-
-def fill_notes(notes: list, target_size: int):
-    """ Given a list of notes, fill the list to target list length
-
-    :param notes: list of notes
-    :param target_size: target_size to fill
-    :return: return list of notes with target size
-    """
-    return "hello world"
-    pass
-
-
-def trim_notes(notes: list, target_size: int):
-    """ Given a list of notes, trim to target size by dropping the last the notes
-
-    :param notes: list of notes
-    :param target_size: target size
-    :return: return list of notes with target size
-    """
-    pass
 
 
 def build_song(song):
