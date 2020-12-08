c142ce5e5c19a6e8ca918d8fd3cab6f00c4ef1bd
Jason Livinghouse <32722492+JasonL24@users.noreply.github.com>
2020-12-07 19:41:28 -0800

    config updates

--- backend/ml_src/firebase_config.py
+++ backend/ml_src/firebase_config.py
@@ -2,5 +2,5 @@ from firebase import firebase
 from firebase_admin import credentials, initialize_app
 fb = firebase.FirebaseApplication("https://ecs171group9.firebaseio.com", None)
 
-cred = credentials.Certificate('./ml_src/ecs171group9-dc4c756adb13')
+cred = credentials.Certificate('./ml_src/ecs171group9-dc4c756adb13.json')
 initialize_app(cred, {'storageBucket': 'ecs171group9.appspot.com'})
